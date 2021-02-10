b1cf69569a5fea73eb9b8782f2877849084ad797
Alexander Raevskiy <aeg2611@gmail.com>
2021-02-10 21:37:35 +0300

    mantis тест добавления и удаления проекта

---
+++ test/test_add_project.py
@@ -0,0 +1,16 @@
+from model.project import Project
+
+
+def test_add_project(app, db, json_project, check_ui):
+    project = json_project
+    old_projects = db.get_project_list()
+    app.project.create(project)
+    new_projects = db.get_project_list()
+    old_projects.append(project)
+
+    def clean(cleaning_project):
+        return Project(identifier=cleaning_project.identifier, name=cleaning_project.name.strip(),
+                       description=cleaning_project.description.strip())
+    if check_ui:
+        clean_new_projects = map(clean, new_projects)
+        assert sorted(clean_new_projects, key=Project.id_or_max) == sorted(app.project.get_project_list(), key=Project.id_or_max)
